a8f2c19704ab1c4c0362fc9b2bee46255fa6f5a4
Brendan Kellam <bshizzle1234@gmail.com>
2019-02-25 17:46:20 -0500

    Modified navigation turning

--- Lab5/src/ca/mcgill/ecse211/lab5/Lab5.java
+++ Lab5/src/ca/mcgill/ecse211/lab5/Lab5.java
@@ -1,32 +1,19 @@
 package ca.mcgill.ecse211.lab5;
 
 import java.io.FileNotFoundException;
-
 import ca.mcgill.ecse211.colour.ColourDetection;
 import ca.mcgill.ecse211.hardware.Vehicle;
-import ca.mcgill.ecse211.lab5.FieldSearch.SearchArea;
 import ca.mcgill.ecse211.localization.FallingEdgeLocalizer;
-import ca.mcgill.ecse211.localization.LightLocalizer;
 import ca.mcgill.ecse211.localization.RisingEdgeLocalizer;
 import ca.mcgill.ecse211.localization.UltrasonicLocalizer;
 import ca.mcgill.ecse211.odometer.Odometer;
 import ca.mcgill.ecse211.odometer.OdometerExceptions;
 import ca.mcgill.ecse211.odometer.OdometryCorrection;
 import ca.mcgill.ecse211.ultrasonic.UltrasonicPoller;
-import ca.mcgill.ecse211.util.Board;
 import ca.mcgill.ecse211.util.Display;
 import ca.mcgill.ecse211.util.Log;
-import ca.mcgill.ecse211.util.Log.Sender;
 import lejos.hardware.Button;
 import lejos.hardware.Sound;
-import lejos.robotics.chassis.Chassis;
-import lejos.robotics.chassis.Wheel;
-import lejos.robotics.chassis.WheeledChassis;
-import lejos.robotics.localization.PoseProvider;
-import lejos.robotics.navigation.Navigator;
-import lejos.robotics.navigation.Pose;
-import lejos.robotics.navigation.MovePilot;
-import lejos.robotics.navigation.ArcAlgorithms;
 
 /**
  * Main entry point class for Lab4
@@ -75,7 +62,7 @@ public final class Lab5 {
 		FieldSearch.StartingCorner SC = FieldSearch.StartingCorner.LOWER_LEFT;
 
 		// Starting corner [0, 3]
-		FieldSearch.SearchArea searchArea = new FieldSearch.SearchArea(LLx, LLy, URx, URy, SC);
+		SearchArea searchArea = new SearchArea(LLx, LLy, URx, URy, SC);
 
 		// Create odometer
 		Odometer odometer = Odometer.getOdometer();
---
+++ Lab5/src/ca/mcgill/ecse211/lab5/SearchArea.java
@@ -0,0 +1,120 @@
+package ca.mcgill.ecse211.lab5;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+import ca.mcgill.ecse211.lab5.FieldSearch.StartingCorner;
+import ca.mcgill.ecse211.util.Board;
+import lejos.robotics.geometry.Point;
+
+public class SearchArea {
+                
+    private Point bottomLeft, bottomRight, topLeft, topRight;
+    private StartingCorner startingCorner;
+    
+    private Queue<Point> centerWaypoints;
+    
+    public SearchArea(int LLx, int LLy, int URx, int URy, StartingCorner SC) {
+                    
+        this.startingCorner = SC;
+        
+        Point bl = new Point(LLx-1, LLy-1);
+        Point br = new Point(URx-1, LLy-1);
+        Point tl = new Point(LLx-1, URy-1);
+        Point tr = new Point(URx-1, URy-1);
+                 
+        // Translate points
+        switch(startingCorner) {
+        
+        case LOWER_LEFT:
+            bottomLeft = bl;
+            bottomRight = br;
+            topLeft = tl;
+            topRight = tr;
+            break;
+            
+        case LOWER_RIGHT:
+            bottomLeft = br;
+            bottomRight = tr;
+            topLeft = bl;
+            topRight = tl;
+            break;
+            
+        case UPPER_RIGHT:
+            bottomLeft = tr;
+            bottomRight = tl;
+            topLeft = br;
+            topRight = bl;
+            break;
+            
+        case UPPER_LEFT:
+            bottomLeft = tl;
+            bottomRight = bl;
+            topLeft = tr;
+            topRight = br;
+            break;
+        }
+                    
+        this.centerWaypoints = new ArrayDeque<>();
+        
+        float xCur;
+        float yCur;
+        double xOff = getBottomLeft().getX() * Board.TILE_SIZE;
+        double yOff = getBottomLeft().getY() * Board.TILE_SIZE;
+        
+        for (int x = 0; x < (int) getWidth(); x++) {
+            
+            if (x % 2 == 0) {
+                for (int y = 0; y < (int) getHeight(); y++) {
+                    
+                    xCur = (float) (x * Board.TILE_SIZE + xOff + Board.TILE_SIZE / 2);
+                    yCur = (float) (y * Board.TILE_SIZE + yOff + Board.TILE_SIZE / 2);
+                    
+                    centerWaypoints.add(new Point(xCur, yCur));
+                    
+                }
+            } else {
+                for (int y = (int) getHeight()-1; y>= 0; y--) {
+                    
+                    xCur = (float) (x * Board.TILE_SIZE + xOff + Board.TILE_SIZE / 2);
+                    yCur = (float) (y * Board.TILE_SIZE + yOff + Board.TILE_SIZE / 2);
+                    
+                    centerWaypoints.add(new Point(xCur, yCur));
+                    }
+                }
+                
+            }
+            
+        }
+        
+        public Point getNextWaypoint() {
+            return centerWaypoints.poll();
+        }
+ 
+        public Point getBottomLeft() {
+            return bottomLeft;
+        }
+        
+        public Point getTopRight() {
+            return topRight;
+        }
+        
+        public Point getBottomRight() {
+            return bottomRight;
+        }
+        
+        public Point getTopLeft() {
+            return topLeft;
+        }
+        
+        public double getHeight() {
+            return this.topLeft.getY() - this.bottomLeft.getY();
+        }
+        
+        public double getWidth() {
+            return this.bottomRight.getX() - this.bottomLeft.getX();
+        }
+
+    }
+    
+
