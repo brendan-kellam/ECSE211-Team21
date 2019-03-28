2ba9e1ef6ce059e63f848bd3182ea4cebcafc098
Brendan Kellam <bshizzle1234@gmail.com>
2019-03-27 23:12:10 -0400

    Added concept of concurrent lineRunners that will drive until a line is detected

--- Source/src/ca/mcgill/ecse211/localization/DualLightLocalizer.java
+++ Source/src/ca/mcgill/ecse211/localization/DualLightLocalizer.java
@@ -5,6 +5,9 @@ import ca.mcgill.ecse211.light.ColorSensor;
 import ca.mcgill.ecse211.navigation.Navigator;
 import ca.mcgill.ecse211.util.Board;
 import ca.mcgill.ecse211.util.Board.Heading;
+import lejos.hardware.Sound;
+import lejos.hardware.motor.BaseRegulatedMotor;
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
 /**
  * Blocking routine to 
@@ -38,29 +41,104 @@ public class DualLightLocalizer {
         // Start by turning to the given heading
         Navigator.turnTo(Board.getHeadingAngle(heading));
         
+        LineRunner l = new LineRunner(leftSensor, Vehicle.LEFT_MOTOR, 100);
+        LineRunner r = new LineRunner(rightSensor, Vehicle.RIGHT_MOTOR, 100);
+        
+        Thread lThread = new Thread(l);
+        Thread rThread = new Thread(r);
+        
+        lThread.start();
+        rThread.start();
+        
+        try {
+            lThread.join();
+            rThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        
+        
+        
+        /*
+        boolean left = false, right = false; 
+        
         // Begin by advancing forward until either the left or right sensor detects a line
         Vehicle.setMotorSpeeds(50, 50);
-        while (!leftSensor.lineDetected() || !rightSensor.lineDetected());
-        
+        while (!( (left = leftSensor.lineDetected()) || (right = rightSensor.lineDetected()) )) {
+            try {
+                Thread.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        Sound.twoBeeps();
+
         // If both sensors detected a line
-        if (leftSensor.lineDetected() && rightSensor.lineDetected()) {
+        if (left && right) {
             Vehicle.setMotorSpeeds(0, 0);
         }
         
         // Left detected
-        else if (leftSensor.lineDetected()) {
-            Vehicle.LEFT_MOTOR.setSpeed(0);
-            while (!rightSensor.lineDetected());
-            Vehicle.RIGHT_MOTOR.setSpeed(0);
+        else if (left) {
+            Vehicle.LEFT_MOTOR.stop();
+            while (!rightSensor.lineDetected()) {
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Vehicle.RIGHT_MOTOR.stop();
         }
         
         // right detected
-        else {
-            Vehicle.RIGHT_MOTOR.setSpeed(0);
-            while (!leftSensor.lineDetected());
-            Vehicle.LEFT_MOTOR.setSpeed(0);
+        else if (right) {
+            Vehicle.RIGHT_MOTOR.stop();
+            while (!leftSensor.lineDetected()) {
+                try {
+                    Thread.sleep(5);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            Vehicle.LEFT_MOTOR.stop();
         }
         
+        */
+        
+    }
+
+    class LineRunner implements Runnable {
+        
+        private ColorSensor sensor;
+        private EV3LargeRegulatedMotor motor;
+        private float speed;
+        
+        public LineRunner(ColorSensor sensor, EV3LargeRegulatedMotor motor, float speed) {
+            this.sensor = sensor;
+            this.motor = motor;      
+            this.speed = speed;
+        }
+
+        @Override
+        public void run() {
+            
+            motor.setSpeed(speed);
+            motor.forward();
+            
+            while (!sensor.lineDetected()) {
+                try {
+                    Thread.sleep(30);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            
+            motor.stop();
+            
+            Sound.beep();
+        }
         
     }
     
